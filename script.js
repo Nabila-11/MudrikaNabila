7d2d008148235efdb86873a6bda1fe54f625c0b0
mudrikanabila <nabilamudrika@gmail.com>
2025-05-16 11:41:23 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -55,3 +55,89 @@ playButton.addEventListener('click', function() {
 pauseButton.addEventListener('click', function() {
     audio.pause(); // Menjeda audio
 });
+
+function setCookie(name, value, days) {
+  var expires = "";
+  if (days) {
+    var date = new Date();
+    date.setTime(date.getTime() + (days*24*60*60*1000));
+    expires = "; expires=" + date.toUTCString();
+  }
+  document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
+
+function getCookie(name) {
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(';');
+  for(var i=0;i < ca.length;i++) {
+    var c = ca[i];
+    while (c.charAt(0)==' ') c = c.substring(1,c.length);
+    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+  }
+  return null;
+}
+
+function acceptCookie() {
+  setCookie('cookieConsent', 'accepted', 365);
+  document.getElementById('cookie-banner').style.display = 'none';
+}
+
+window.onload = function() {
+  if (!getCookie('cookieConsent')) {
+    document.getElementById('cookie-banner').style.display = 'block';
+  }
+};
+
+function setCookie(name, value, days) {
+  var expires = "";
+  if (days) {
+    var date = new Date();
+    date.setTime(date.getTime() + (days*24*60*60*1000));
+    expires = "; expires=" + date.toUTCString();
+  }
+  document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
+function getCookie(name) {
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(';');
+  for(var i=0;i < ca.length;i++) {
+    var c = ca[i];
+    while (c.charAt(0)==' ') c = c.substring(1,c.length);
+    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+  }
+  return null;
+}
+function acceptCookie() {
+  setCookie('cookieConsent', 'accepted', 365);
+  setCookie('analytics', 'yes', 365);
+  setCookie('ads', 'yes', 365);
+  document.getElementById('cookie-banner').classList.add('hide');
+}
+function openCookieModal() {
+  document.getElementById('cookie-modal').classList.add('active');
+  // Set checkbox sesuai cookie
+  document.getElementById('analytics-cookie').checked = getCookie('analytics') !== 'no';
+  document.getElementById('ads-cookie').checked = getCookie('ads') !== 'no';
+}
+function closeCookieModal() {
+  document.getElementById('cookie-modal').classList.remove('active');
+}
+document.getElementById('cookieForm').onsubmit = function(e) {
+  e.preventDefault();
+  setCookie('cookieConsent', 'custom', 365);
+  setCookie('analytics', document.getElementById('analytics-cookie').checked ? 'yes' : 'no', 365);
+  setCookie('ads', document.getElementById('ads-cookie').checked ? 'yes' : 'no', 365);
+  closeCookieModal();
+  document.getElementById('cookie-banner').classList.add('hide');
+};
+window.onload = function() {
+  if (!getCookie('cookieConsent')) {
+    document.getElementById('cookie-banner').classList.remove('hide');
+  } else {
+    document.getElementById('cookie-banner').classList.add('hide');
+  }
+};
+// Tutup modal jika klik di luar modal
+document.getElementById('cookie-modal').onclick = function(e) {
+  if (e.target === this) closeCookieModal();
+};
